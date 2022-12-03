c03a6941a2e2e4b27b6cdd98a8d16271dc8e3e2e
Marcos <jmarcoshenrich@gmail.com>
2022-12-03 15:21:27 -0800

    info panels made modular

--- OgreSlayer/src/gameView.js
+++ OgreSlayer/src/gameView.js
@@ -6,6 +6,7 @@ export default class GameView {
 
         this.CANVAS_WIDTH = canvas.width = 1024
         this.CANVAS_HEIGHT = canvas.height = 760
+        this.infoDimensions = { infoSquareYOffset: 200, infoSquareXOffset: 274, infoSquareLen: 200, infoSquareHeight: 400 } 
 
         this.backgroundImage = new Image()
         this.backgroundImage.src = 'art/arena.jpg'
@@ -34,26 +35,27 @@ export default class GameView {
     }
 
     renderBackground() {
-        this.ctx.drawImage(this.backgroundImage, 0, 0, 1024, 768)
-
+        this.ctx.drawImage(this.backgroundImage, 0, 0, this.CANVAS_WIDTH, this.CANVAS_HEIGHT)
+       
         this.ctx.fillStyle = 'rgba(225,225,225,0.9)';
-        this.ctx.fillRect(750, 200, 200, 400)
+        const opponentInfoSquare = this.ctx.fillRect((this.CANVAS_WIDTH - this.infoDimensions.infoSquareXOffset), this.infoDimensions.infoSquareYOffset, this.infoDimensions.infoSquareLen, this.infoDimensions.infoSquareHeight)
        
         let opponentMove = this.game.opponent.nextMove[0].art 
-        this.ctx.drawImage(opponentMove, 780, 350, 120, 200)
+        this.ctx.drawImage(opponentMove, (this.CANVAS_WIDTH - this.infoDimensions.infoSquareXOffset) + 40, this.infoDimensions.infoSquareYOffset + 160, 120, 200)
 
-        this.ctx.fillRect(90, 200, 200, 400)
+        const playerInfoSquare = this.ctx.fillRect((this.infoDimensions.infoSquareXOffset - this.infoDimensions.infoSquareLen), this.infoDimensions.infoSquareYOffset, this.infoDimensions.infoSquareLen, this.infoDimensions.infoSquareHeight)
     }
 
     renderText() {
+        // { infoSquareYOffset: 200, infoSquareXOffset: 274, infoSquareLen: 200, infoSquareHeight: 400 } 
         this.ctx.fillStyle = 'rgba(0,0,0,1)';
         this.ctx.font = "bold 20px verdana, sans-serif "
-        this.ctx.fillText("Monster Health", 765, 240)
-        this.ctx.fillText("Move", 810, 335)
-        this.ctx.fillText(this.game.opponent.health, 830, 300)
+        this.ctx.fillText("Monster Health", (this.CANVAS_WIDTH - this.infoDimensions.infoSquareXOffset) + 14, this.infoDimensions.infoSquareYOffset + 40)
+        this.ctx.fillText("Move", (this.CANVAS_WIDTH - this.infoDimensions.infoSquareXOffset) + 65, this.infoDimensions.infoSquareYOffset + 140)
+        this.ctx.fillText(this.opponent.health, (this.CANVAS_WIDTH - this.infoDimensions.infoSquareXOffset) + 80, this.infoDimensions.infoSquareYOffset + 80)
 
-        this.ctx.fillText("Knight Health", 110, 240)
-        this.ctx.fillText(this.game.knight.health, 175, 300)
+        this.ctx.fillText("Knight Health", (this.infoDimensions.infoSquareXOffset - this.infoDimensions.infoSquareLen) + 21, this.infoDimensions.infoSquareYOffset + 40)
+        this.ctx.fillText(this.game.knight.health, (this.infoDimensions.infoSquareXOffset - this.infoDimensions.infoSquareLen) + 81, this.infoDimensions.infoSquareYOffset + 80)
     }
 
 }
